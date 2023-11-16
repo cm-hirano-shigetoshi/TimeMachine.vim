6e4f81b48a04ad4a031a660de1828d2076f4d7c0
hirano.shigetoshi <hirano.shigetoshi@classmethod.jp>
2023-11-17 01:29:22 +0900

    git logの一覧から選ぶようにした

--- lua/TimeMachine.lua
+++ lua/TimeMachine.lua
@@ -36,8 +36,8 @@ local function get_buf_name()
     end
 end
 
-local function get_hash_ids()
-    local cmd = "git log --oneline " .. get_buf_name() .. " | awk '{print $1}'"
+local function get_hash_ids(buf_name)
+    local cmd = "git log --oneline " .. buf_name .. " | awk '{print $1}'"
     return split(vim.fn.system(cmd), "\n")
 end
 
@@ -45,18 +45,32 @@ local function get_output_path(dirname, hash_id)
     return dirname .. "/" .. hash_id
 end
 
-local function save_snapshot(hash_id, output_path)
-    local cmd = "git show " .. hash_id .. ":$(git rev-parse --show-prefix)" .. get_buf_name() .. " > " .. output_path
+local function save_snapshot(buf_name, hash_id, output_path)
+    local cmd = "git show " .. hash_id .. ":$(git rev-parse --show-prefix)" .. buf_name .. " > " .. output_path
     os.execute("mkdir -p " .. output_path:match("(.*/)"))
     os.execute(cmd)
 end
 
-local function dump_histories(dirname)
+local function get_hash_id(result)
+    local pattern = "%w%w%w%w%w%w%w*"
+    local match = string.match(result, pattern)
+    if match then
+        return match
+    end
+    return ""
+end
+
+local function dump_history(buf_name, hash_id, dirname)
+    local output_path = get_output_path(dirname, hash_id)
+    save_snapshot(buf_name, hash_id, output_path)
+    return output_path
+end
+
+local function dump_histories(buf_name, dirname)
     os.execute("rm -fr " .. dirname)
-    local hash_ids = get_hash_ids()
+    local hash_ids = get_hash_ids(buf_name)
     for _, hash_id in ipairs(hash_ids) do
-        local output_path = get_output_path(dirname, hash_id)
-        save_snapshot(hash_id, output_path)
+        dump_history(buf_name, hash_id, dirname)
     end
 end
 
@@ -74,27 +88,37 @@ local function get_positions_to_open(dirname, results)
     return positions
 end
 
-local function execute_fzf(dirname_, query_)
-    coroutine.wrap(function(dirname, query)
-        local results = fzf.fzf("(cd " .. dirname .. " && rg --color always -L -n ^)",
-            "--multi --ansi --reverse --delimiter ':' --query '" .. query .. "' " ..
-            "--preview '(cd " .. dirname .. " && bat --plain --number --color always --highlight-line {2} {1})' " ..
-            "--preview-window 'right:60%' --preview-window '+{2}+1/2'")
+local function execute_fzf(buf_name_, query_, tmpdir_)
+    coroutine.wrap(function(buf_name, query, tmpdir)
+        local results = fzf.fzf("git log --oneline --graph --color=always " .. buf_name,
+            "--multi --ansi --reverse --query '" .. query .. "'")
         if results then
+            for _, result in ipairs(results) do
+                local hash_id = get_hash_id(result)
+                if hash_id then
+                    os.execute("rm -fr " .. tmpdir)
+                    local output_path = dump_history(buf_name_, hash_id, tmpdir)
+                    vim.api.nvim_command("next " .. output_path)
+                end
+            end
+            --[[
             local positions_to_open = get_positions_to_open(dirname, results)
             for _, position in ipairs(positions_to_open) do
                 vim.api.nvim_command("next " .. position[1])
                 vim.api.nvim_win_set_cursor(0, { position[2], 0 })
             end
+            ]]
+            --
         end
         --os.execute("rm -fr " .. dirname)
-    end)(dirname_, query_)
+    end)(buf_name_, query_, tmpdir_)
 end
 
 local function call(query)
     local tmpdir = os.tmpname()
-    dump_histories(tmpdir)
-    execute_fzf(tmpdir, query)
+    local buf_name = get_buf_name()
+    --dump_histories(buf_name, tmpdir)
+    execute_fzf(buf_name, query, tmpdir)
 end
 
 M.run = function(query)
